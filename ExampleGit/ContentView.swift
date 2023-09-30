0faad4f4a64bf48e10949f7d6fa3bcc212a90648
Abraham Morales Arroyo <abrahamof606@gmail.com>
2023-09-30 11:47:50 -0500

    fixed the issue on line 17

--- ExampleGit/ContentView.swift
+++ ExampleGit/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, World!")
-            Button("hello", Action: pressedHi())
+            Button("hello", action: pressedHi)
             
         }
         .padding()
