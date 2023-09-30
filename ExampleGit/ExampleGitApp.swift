f3d2846003950e62c4a97fe67876e9f474af4fa4
Abraham Morales Arroyo <abrahamof606@gmail.com>
2023-09-30 11:10:25 -0500

    Initial Commit

---
+++ ExampleGit/ExampleGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleGitApp.swift
+//  ExampleGit
+//
+//  Created by Abraham Morales Arroyo on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExampleGitUITests/ExampleGitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ExampleGitUITestsLaunchTests.swift
+//  ExampleGitUITests
+//
+//  Created by Abraham Morales Arroyo on 9/30/23.
+//
+
+import XCTest
+
+final class ExampleGitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
